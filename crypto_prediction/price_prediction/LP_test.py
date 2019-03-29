1ee965bd33777acf3444ec8fe8abca98b07f75b8
Jonathan Cho <jon88cho@gmail.com>
2019-03-28 21:43:43 -0400

    today's price and LP

--- crypto_prediction/price_prediction/LP_test.py
+++ crypto_prediction/price_prediction/LP_test.py
@@ -5,39 +5,44 @@ import pandas as pd
 
 random.seed(a=10)
 # c = {(i): 10*i for i in range(1,11)} #original purchase price
-# p = {(i): 10*i + random.randint(1,7) for i in range(1,11)} #predicted price
+# p = {(i): 10*i + random.randint(-7,7) for i in range(1,11)} #predicted price
 # B = 1000 # budget
-# f = {(i): random.randint(1,10) for i in range(1,11)} # fraction
+# f = {(i): random.randint(1,10) for i in range(1,11)} # Maximum
+y={'bitcoin':10}
+x=['bitcoin']
+c = {'bitcoin':20}
+p = {'bitcoin':10}
+B = 1000
+f = {'bitcoin':20}
 def get_optimal_quantities(y: {'coin name' : 'amount_held'}, x: ['coin names'], c: {'coin name' : 'cost'}, p : {'coin name' : 'current price'}, B : 'Int: budget', f : {'coin name': 'max fraction in portfolio'}) -> {'coin name': 'quantity'}:
     
     problem = plp.LpProblem("Crypto-currency porfolio", sense=plp.LpMaximize)
-    x = {(i):plp.LpVariable(cat=plp.LpContinuous, name="x{0}".format(i)) for i in x}
+    x_var = {(i):plp.LpVariable(cat=plp.LpContinuous, name="x{0}".format(i)) for i in x}
 
     problem += plp.LpConstraint(
-                 e=plp.lpSum(x[i] * p[i] for i in x),
+                 e=plp.lpSum(x_var[i] * p[i] for i in x),
                  sense=plp.LpConstraintLE,
                  rhs=B,
                  name="budget")
-    for i in x:
+    for i in x_var:
         problem += plp.LpConstraint(
-                    e=x[i],
+                    e=x_var[i],
                     sense=plp.LpConstraintLE,
-                    rhs=(B*f[i]/100)/p[i],
+                    rhs=f[i],
                     name="limit_{0}".format(i))
         problem += plp.LpConstraint(
-                    e=x[i],
+                    e=x_var[i],
                     sense=plp.LpConstraintGE,
                     rhs=-y[i],
                     name="amount_constraint{0}".format(i))
 
-    objective = plp.lpSum(x[i] * (p[i]-c[i]) for i in x)
+    objective = plp.lpSum(x_var[i] * (p[i]-c[i]) for i in x)
     problem.setObjective(objective)
     problem.solve()
     print(plp.LpStatus[problem.status])
     # opt_df = pd.DataFrame.from_dict(x, orient="index", columns = ["variable_object"])
     # opt_df["solution_value"] = opt_df["variable_object"].apply(lambda item: item.varValue)
-    results = {i: x[i].varValue for i in x}
-    
+    results = {i: x_var[i].varValue for i in x_var}
     return results
 
-# print(get_optimal_quantities([i for i in range(1,10)],c,p,B,f))
+print(get_optimal_quantities(y,x,c,p,B,f))
--- crypto_prediction/price_prediction/crypto_prices.py
+++ crypto_prediction/price_prediction/crypto_prices.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ssl
+import time
 
 ssl._create_default_https_context = ssl._create_unverified_context
 def getTodaysPricesFor(coinList):
@@ -8,4 +9,13 @@ def getTodaysPricesFor(coinList):
         dataframe = pd.read_html("https://coinmarketcap.com/currencies/" + coin + "/", flavor='html5lib')[0]
         print(dataframe)
 
-getTodaysPricesFor(list(["bitcoin"]))
+# getTodaysPricesFor(["bitcoin"])
+
+def get_tomorrow_price (coin):
+    #   Method to download data
+    dataframe = pd.read_html("https://coinmarketcap.com/currencies/" + coin +
+                             "/historical-data/?start="+time.strftime("%Y%m%d")+"&end="+time.strftime("%Y%m%d"), flavor='html5lib')[0]
+    dataframe.columns = ["Date","Open","High","Low","Close","Volume","Market_Cap"]
+    return dataframe['Close'][0]
+
+print(get_tomorrow_price('bitcoin'))
